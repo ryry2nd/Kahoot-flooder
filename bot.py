f9e9895b21c22a7ffa50f59d7ee09e561449d888
Ryan Stouffer2 <ryry2nd@gmail.com>
2022-12-05 10:16:38 -0500

    flooder

--- bot.py
+++ bot.py
@@ -1,13 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
 def flood(numBots, id, name=""):
     options = webdriver.ChromeOptions()
-    #options.add_argument('headless')
+    options.add_argument('headless')
 
     for botNum in range(numBots):
         driver = webdriver.Chrome(executable_path="chromedriver.exe", options=options)
 
-        driver.get("https://kahoot.it/")
+        driver.get("https://kahoot.it?pin=" + id + "&refer_method=link")
+
+        time.sleep(0.5)
+
+        nameBox = driver.find_element(By.XPATH, "//input[@name='nickname']")
+        
+        nameBox.send_keys(name + str(botNum))
+        nameBox.send_keys(Keys.ENTER)
 
         yield driver
--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from selenium.webdriver.common.by import By
 from bot import flood
 import json, os
 
-for bot in flood(10, 1234, "boobs"):
+for bot in flood(100, "2014022", "bigPP"):
     pass
